cfadc8a2168a7b4c381208f6ff12969d7a2faccc
Edison Avilés <eaviles@novaseguroslatam.com>
2024-02-22 09:50:30 -0500

    fix exports

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,11 +1,13 @@
 import BaseModel from "./base.model"
 import * as BaseSchema from "./base.schema"
 import mongooseModel from "./mongoose.model"
-import * as Repository from "./repository"
+import Repository from "./repository"
+import IRepository from "./i_repository"
 
 export {
     BaseModel,
     BaseSchema,
     mongooseModel,
-    Repository
+    Repository,
+    IRepository
 }
