77c072ff6ef08c37be993be6ca5625d9e199a818
Edison Avilés <eaviles@novaseguroslatam.com>
2024-03-27 12:11:19 -0500

    Build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "tsup"
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-})
+  entry: {
+    index: "src/index.ts",
+  },
+  external: [],
+  format: ["esm", "cjs"],
+  esbuildPlugins: [],
+  dts: "src/index.ts",
+});
