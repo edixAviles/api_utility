e174772a1a478fc0e5ad56ec7bded5130e602689
Edison Avilés <eaviles@novaseguroslatam.com>
2024-03-28 12:13:35 -0500

    Update build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: {
-    index: "src/index.ts",
-  },
-  external: [],
-  format: ["esm", "cjs"],
-  esbuildPlugins: [],
-  dts: "src/index.ts",
+  target: "es2020",
+  format: ["cjs", "esm"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: true
 })
